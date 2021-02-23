6e289b356be2b0df3fddbda70d741b48a0265652
Juan Jimenez <jlja2@ensign.edu>
2021-02-22 20:26:47 -0700

    LandingPage

---
+++ controllers/index.js
@@ -0,0 +1,6 @@
+
+// module for index
+
+exports.index = function(req, res, next) {
+    res.render('index', { title: 'A corre lo Leakers' });
+  }
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/*modular instance of home or index*/
+
+let index = require('../controllers/index.js');
+
+/* GET home page. */
+router.get('/', index.index );
+
+module.exports = router;
+
