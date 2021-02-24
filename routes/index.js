915f1958cfa613959ac6dde164feff42c5a784bc
juanluisja1 <jlja2@ensign.edu>
2021-02-23 17:20:22 -0700

    landing page post form

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,12 @@ var router = express.Router();
 
 let landing = require('../controllers/landing.js');
 
+//Routes
+
 /* GET home page. */
 router.get('/', landing.get_landing );
-
+/* Post form*/
+router.post('/', landing.submit_lead);
 module.exports = router;
 
 //DEBUG=myapp:* npm start
